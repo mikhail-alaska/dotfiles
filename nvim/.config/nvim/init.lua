1245f04256962457b22d1f9195d885233667dba2
uncom1c <mishartem2004@gmail.com>
2024-09-08 15:41:54 +0300

    zsh

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,5 +2,5 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-vim.wo.number = true
+vim.wo.rnu= true
 require("config.lazy")
