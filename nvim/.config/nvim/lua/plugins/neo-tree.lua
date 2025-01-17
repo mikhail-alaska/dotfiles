4681f7d75a2e3a21c2fd557e672b9b2e1af84a8c
uncom1c <mishartem2004@gmail.com>
2025-01-17 07:56:23 +0300

    neotree dotfiles shown

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,29 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
+    opts = {
+      filesystem = {
+        filtered_items = {
+       visible = true,
+       show_hidden_count = true,
+       hide_dotfiles = false,
+       hide_gitignored = true,
+       hide_by_name = {
+         -- '.git',
+         -- '.DS_Store',
+         -- 'thumbs.db',
+       },
+      never_show = {},
+        },
+      }
+    },
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	config = function()
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
