0dd0878ee07f5c14c042a08891f781820c560e26
uncom1c <mishartem2004@gmail.com>
2025-02-24 15:00:37 +0300

    Scripted auto-commit on change (2025-02-24 15:00:37) by gitwatch.sh

--- nvim/.config/nvim/lua/plugins/ccatppuccin.lua
+++ nvim/.config/nvim/lua/plugins/ccatppuccin.lua
@@ -1,23 +1,32 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-        integrations = {
-            nvimtree = true,
-            treesitter = true,
-        },
-        color_overrides = {
-            all = {
-                surface1 = "#11111b",
-            },
-        },
-      })
-      vim.cmd.colorscheme "catppuccin-mocha"
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+                integrations = {
+                    nvimtree = true,
+                    treesitter = true,
+                },
+                color_overrides = {
+                    all = {
+                        surface1 = "#11111b",
+                    },
+                },
+            })
+            vim.cmd.colorscheme "catppuccin-mocha"
+        end
+    },
+    opts = {
+        -- regular opts
+    },
+    config = function(_, opts)
+        require('notify').setup(vim.tbl_extend('keep', {
+            -- other stuff
+            background_colour = "#000000"
+        }, opts))
     end
-  }
 }
