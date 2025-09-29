448fb9ae7f35b66e6bb0bbbaedb038d16ce6c52b
uncom1c <mishartem2004@gmail.com>
2025-09-29 13:15:09 +0300

    Scripted auto-commit on change (2025-09-29 13:15:08) by gitwatch.sh

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -115,8 +115,8 @@ local function create_floating_window(opts)
 
   -- Create the floating window
   local win = vim.api.nvim_open_win(buf, true, win_config)
-    vim.api.nvim_win_set_option(win, "winhl", "Normal:NormalFloat")
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_win_set_option(win, "winhl", "Normal:NormalFloat")
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   return { buf = buf, win = win }
 end
 
