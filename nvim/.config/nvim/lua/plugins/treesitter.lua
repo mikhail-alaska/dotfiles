e33541fceb11f0719feaf05f52eff12ec7df2989
uncom1c <mishartem2004@gmail.com>
2026-05-10 16:11:30 +0300

    Scripted auto-commit on change (2026-05-10 16:11:30) by gitwatch.sh

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,8 +7,21 @@ return {
       config.setup({
         ensure_installed = {"c", "go", "lua", "vim", "vimdoc", "query", "cpp", "python", "markdown", "markdown_inline"},
         auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
+        highlight = {
+          enable = true,
+          disable = { "markdown", "markdown_inline" },
+        },
+        indent = {
+          enable = true,
+          disable = { "markdown" },
+        },
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "markdown", "vimwiki" },
+        callback = function(args)
+          pcall(vim.treesitter.stop, args.buf)
+        end,
       })
     end
   }
